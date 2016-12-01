5e6c92ccb5c9093573a30636c93051b2b4641361
Sanjiv Upadhyaya <sanjivupadhyaya@Sanjivs-MacBook-Pro.local>
2016-12-01 15:26:59 +0100

    changes for CI completed

--- core/importer.py
+++ core/importer.py
@@ -26,7 +26,7 @@ class Importer(Thread):
 
 
     def run(self):
-        
+
         try:
             conn_string = param.conn_bi
             conn = psycopg2.connect(conn_string)
@@ -63,8 +63,8 @@ class Importer(Thread):
             param.counter-1
 
             conn.rollback()
-            curs.execute("""INSERT INTO etl_status (start_date, end_date, schema_name, table_name, error_phase, error_message) 
-            VALUES(%s, %s, %s, %s, %s, %s)""",[param.start_date, param.end_date, param.schema, self.file_name, 'import', str(e)])
+            curs.execute("""INSERT INTO etl_status (start_date, end_date, schema_name, table_name, error_phase, error_message, status) 
+            VALUES(%s, %s, %s, %s, %s, %s, %s)""",[param.start_date, param.end_date, param.schema, self.file_name[:-4], 'import', str(e),'fail'])
             conn.commit()
             
         finally:
