5fe6857838509899c950925853f4d8c16166654b
Sanjiv Upadhyaya <sanjivupadhyaya@Sanjivs-MacBook-Pro.local>
2016-12-05 13:55:26 +0100

    cron and log added for etl failure processes

--- miscellaneous/crons/cron_status.sh
+++ miscellaneous/crons/cron_status.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 source /opt/credential/pass.sh && python /opt/etl/miscellaneous/notify.py >> /tmp/log_status$$.log 2>&1
-mailx -s "ETL Failure Notification" sanjiv.upadhyaya@shore.com < /tmp/log_status$$.log
 cat /tmp/log_status$$.log >> /opt/etl/core/logs/log_status.txt
 rm /tmp/log_status$$.log
