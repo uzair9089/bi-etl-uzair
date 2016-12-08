97d42c4a7625dc00659bbc72b14893bb481b5fce
Sanjiv Upadhyaya <sanjivupadhyaya@Sanjivs-MacBook-Pro.local>
2016-12-08 09:40:13 +0100

    testing

--- core/exporter.py
+++ core/exporter.py
@@ -35,8 +35,15 @@ class Exporter(Thread):
 
             outputquery = "COPY ({0}) TO STDOUT WITH CSV HEADER".format(self.query)
 
-            with open(param.newpath+self.file_name +".csv", 'w+') as f:
-                curs.copy_expert(outputquery, f)
+            try:
+                with open(param.newpath+self.file_name +".csv", 'w+') as f:
+                    curs.copy_expert(outputquery, f)
+                    
+            except Exception as e:
+                conn.rollback()
+                curs.execute("""INSERT INTO etl_status (start_date, end_date, schema_name, table_name, file_path, error_phase, error_message, status) 
+                VALUES(%s, %s, %s, %s, %s, %s, %s, %s)""",[param.start_date, param.end_date, param.schema, self.file_name[:-4], param.newpath+self.file_name, 'export', str(e), 'fail'])
+                conn.commit()
 
             param.exported_file[self.file_name] = 1
 
