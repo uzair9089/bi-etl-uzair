3b3243fdf78a74b9c7b8efb714a9e713209304e9
Sanjiv Upadhyaya <sanjivupadhyaya@Sanjivs-MacBookPro-2.local>
2017-02-26 19:21:52 +0100

    testing the etl for making the process parameterized

--- testing_automation/runner.py
+++ testing_automation/runner.py
@@ -12,13 +12,14 @@ import threading
 import psycopg2
 import importer
 import datetime
+import copy
 import time
 import sys
 import os
 
-
-# argument passed to runner program is stored in host variable
+# argument passed to #runner program is stored in host variable
 host = sys.argv[1]
+#host = 'intercom'
 
 param.dbconn(host)
 
@@ -32,102 +33,96 @@ DEC2FLOAT = psycopg2.extensions.new_type(
     lambda value, curs: int(value) if value is not None else None)
 psycopg2.extensions.register_type(DEC2FLOAT)
 
+
 # populate the source, truncating behaviour and table renaming schemes from the param file
 def fetch_table(source_name):
+    tbl_source = copy.copy(param.table_hash[source_name][0]['tbl_source'])
 
-	tbl_source = param.table_hash[source_name][0]['tbl_source']
+    tbl_source_truncate = copy.copy(param.table_hash[source_name][1]['tbl_source_truncate'])
 
-	tbl_source_truncate =  param.table_hash[source_name][1]['tbl_source_truncate']
+    tbl_source_rename = copy.copy(param.table_hash[source_name][2]['tbl_source_rename'])
 
-	tbl_source_rename = param.table_hash[source_name][2]['tbl_source_rename']
+    tbl_all = copy.copy(tbl_source) + copy.copy(tbl_source_truncate)
 
-	tbl_all = tbl_source + tbl_source_truncate
+    param.tbl_source = copy.copy(tbl_source)
 
-	param.tbl_source = tbl_source
+    param.tbl_source_truncate = copy.copy(tbl_source_truncate)
 
-	param.tbl_source_truncate = tbl_source_truncate
+    param.tbl_source_rename = copy.copy(tbl_source_rename)
 
-	param.tbl_source_rename = tbl_source_rename
+    # setting tables which needs to be truncatted where table contains no date attributes
+    truncate_table = []
 
-	# setting tables which needs to be truncatted where table contains no date attributes
-	for table in tbl_source_truncate:
-		if table in tbl_source_rename.keys():
-			#for table in tbl_source_truncate:
-			tbl_source_truncate.remove(table)
-			tbl_source_truncate.append(tbl_source_rename[table])
-		else:
-			pass
+    for table in tbl_source_truncate:
+        if table in tbl_source_rename.keys():
+            truncate_table.append(tbl_source_rename[table])
+        else:
+            truncate_table.append(table)
 
-	
-	# for adjusting the table name if it is to be renamed in the BI DWH because of potential similar name clash
-	for table in tbl_all:
-		if table in tbl_source_rename.keys():
-			for table in tbl_source_rename.keys():
-				tbl_all.remove(table)
-				tbl_all.append(tbl_source_rename[table])
-		else:
-			pass
+    # for adjusting the table name if it is to be renamed in the BI DWH because of potential similar name clash
+    for table in tbl_all:
+        if table in tbl_source_rename.keys():
+            for table in tbl_source_rename.keys():
+                tbl_all.remove(table)
+                tbl_all.append(tbl_source_rename[table])
+        else:
+            pass
 
-	param.exported_file = dict((el,0) for el in tbl_all)
-	param.truncate_tbl = tbl_source_truncate
-	#print(tbl_source_truncate)
+    
+    param.exported_file = dict((el, 0) for el in tbl_all)
+    #param.truncate_tbl = tbl_source_truncate
+    param.truncate_tbl = copy.copy(truncate_table)
+    #print(tbl_source_truncate)
+
+    #print(param.exported_file)
+    #print param.truncate_tbl
+
 
 if not os.path.exists(param.newpath):
     os.makedirs(param.newpath)
 
 
-# put the hard coded lines in the param list
-if sys.argv[1] in param.sources:
-	if param.reset_time == param.reset_value:
-		print("Resetting data from " +str(param.reset_start_date) +" - " +str(param.reset_end_date))
-	else:
-		print("Running ETL for " +str(param.start_date) +" - " +str(param.end_date))
-
-
-
 # filter_row/ filter_row_segment is used to filter the data based on the ETL start_date and end_date
 if param.reset_time == param.reset_value:
-	filter_row = " where updated_at >='" +str(param.reset_start_date) +"' and updated_at<'" +str(param.reset_end_date) +"'"
+    filter_row = " where updated_at >='" + str(param.reset_start_date) + "' and updated_at<'" + str(
+        param.reset_end_date) + "'"
 else:
-	filter_row = " where updated_at >='" +str(param.start_date) +"' and updated_at<'" +str(param.end_date) +"'"
-#filter_row = " "
-filter_row_segment =  " where updated_at::date >= current_date::date -1 and updated_at::date < current_date::date "
-
+    filter_row = " where updated_at >='" + str(param.start_date) + "' and updated_at<'" + str(param.end_date) + "'"
+# filter_row = " "
+filter_row_segment = " where updated_at::date >= current_date::date -1 and updated_at::date < current_date::date "
 
 # if host in param.list_of_available_sources
 if (host in param.sources):
-	fetch_table(host)
-	param.counter = len(param.tbl_source) + len(param.tbl_source_truncate)
+    fetch_table(host)
+    param.counter = len(param.tbl_source) + len(param.tbl_source_truncate)
 
-	print(param.truncate_tbl)
-	print(param.exported_file)
 
-	for i in param.tbl_source:
-		print('extraction of ' +i + ' started')
-		# handle the table renaming while importing the table
+    for i in param.tbl_source:
+        #print('extraction of ' + i + ' started')
+        # handle the table renaming while importing the table
 
-		if i in param.tbl_source_rename:
-			runner = Exporter("select * from " + i + filter_row, param.tbl_source_rename[i]) #need to tackle the renamed tables
-			runner.start()
-			#print('select * from '+ i, param.tbl_source_rename[i])
+        if i in param.tbl_source_rename:
+            runner = Exporter("select * from " + i + filter_row, param.tbl_source_rename[i]) #need to tackle the renamed tables
+            runner.start()
+            #print('select * from ' + i, param.tbl_source_rename[i])
 
-		else:
-			runner = Exporter("select * from " + i + filter_row, i) #need to tackle the renamed tables
-			runner.start()
-			#print('select * from '+ i,i)
+        else:
+        	runner = Exporter("select * from " + i + filter_row, i) #need to tackle the renamed tables
+            runner.start()
+            #print('select * from ' + i, i)
 
-	for j in param.tbl_source_truncate:
-		print('extraction of ' + j + ' started')
+    for j in param.tbl_source_truncate:
+        #print('extraction of ' + j + ' started')
 
-		if j in param.tbl_source_rename:
-			runner2 = Exporter('select * from '+ j, param.tbl_source_rename[j])
-			runner2.start()
-			#print('select * from '+ j, param.tbl_source_rename[j])
+        if j in param.tbl_source_rename:
+            runner2 = Exporter('select * from '+ j, param.tbl_source_rename[j])
+            runner2.start()
+            #print('select * from ' + j, param.tbl_source_rename[j])
 
-		else:
-			runner2 = Exporter('select * from '+ j, j)
-			runner2.start()
-			#print('select * from '+ j, j)
+        else:
+            runner2 = Exporter('select * from '+ j, j)
+            runner2.start()
+            #print('select * from ' + j, j)
 
 
 # run the ETL process until all the mentioned tables in the param file are exported.
