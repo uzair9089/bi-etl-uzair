ff8e2fe29184d167206162eca707b96173dfa956
Sanjiv Upadhyaya <sanjivupadhyaya@Sanjivs-MacBookPro-2.local>
2017-02-27 19:45:54 +0100

    test

--- testing_automation/crons/cron_pymt.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-source /opt/credential/pass.sh && python /opt/etl/core/runner.py pymt  >> /tmp/log_pymt$$.log 2>&1
-mailx -s "ETL Status - Payment" sanjiv.upadhyaya@shore.com < /tmp/log_pymt$$.log
-cat /tmp/log_pymt$$.log >> /opt/etl/core/logs/log_pymt.txt
-rm /tmp/log_pymt$$.log
