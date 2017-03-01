550a6b50e6ffe610883109f45b22eec766f3530b
Sanjiv Upadhyaya <sanjivupadhyaya@Sanjivs-MacBookPro-2.local>
2017-03-01 14:52:48 +0100

    test

--- v2_core/runner.py
+++ v2_core/runner.py
@@ -39,9 +39,9 @@ if not os.path.exists(param.newpath):
 # #printing useful information in the std output
 if sys.argv[1] in param.sources:
     if param.reset_time == param.reset_value:
-        #print("Resetting data from " +str(param.reset_start_date) +" - " +str(param.reset_end_date))
+        print("Resetting data from " +str(param.reset_start_date) +" - " +str(param.reset_end_date))
     else:
-        #print("Running ETL for " +str(param.start_date) +" - " +str(param.end_date))
+        print("Running ETL for " +str(param.start_date) +" - " +str(param.end_date))
 
 # filter_row/ filter_row_segment is used to filter the data based on the ETL start_date and end_date
 if param.reset_time == param.reset_value:
