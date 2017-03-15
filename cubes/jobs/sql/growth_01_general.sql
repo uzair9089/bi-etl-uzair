4974e3699372dd2a63401d25d8faeb55af0d8c5a
Sanjiv Upadhyaya <sanjivupadhyaya@Sanjivs-MacBookPro-2.local>
2017-03-15 17:35:52 +0100

    adding new script for the growth general cubes

--- cubes/jobs/sql/growth_01_general.sql
+++ cubes/jobs/sql/growth_01_general.sql
@@ -201,23 +201,131 @@ CREATE INDEX list_subscription_channel_idx
   USING btree
   (id);
 
+DROP TABLE IF EXISTS temp_mrr_range;
+CREATE TEMP TABLE IF NOT EXISTS temp_mrr_range
+(
+  id serial,
+  lower_mrr_range numeric,
+  higher_mrr_range numeric,
+  mrr_range_hlp text
+);
+INSERT INTO temp_mrr_range
+(
+lower_mrr_range,
+higher_mrr_range
+)
+SELECT
+mrr_threshold,
+mrr_threshold + 50
+FROM generate_series(0::int, 99950::int, 50) mrr_threshold;
+
+UPDATE temp_mrr_range
+SET mrr_range_hlp = lower_mrr_range::text || '€ - ' || higher_mrr_range || '€';
+
+DROP TABLE IF EXISTS dmart.list_subscription_mrr;
+CREATE TABLE dmart.list_subscription_mrr AS (SELECT item.onb2__subscription__c as id, 
+ROUND(SUM(	CASE WHEN item.ONB2__StartDate__c::date <= now()::date 
+		AND item.ONB2__EndDate__c::date >= now()::date
+		AND item.ONB2__Active__c = true
+		AND item.ONB2__BillingType__c = 'Recurring'
+	THEN item.fmldiscountedmrrconverted__c
+	ELSE 0
+	END)::numeric, 2) as mrr
+from report.dmart_converted_onb2__item__c item
+inner join prod.onb2__subscription__c subscription
+on subscription.id = item.onb2__subscription__c
+where subscription.onb2__status__c IN ('Active', 'Terminated in Due Time')
+group by item.onb2__subscription__c);
+
+ALTER TABLE dmart.list_subscription_mrr ADD COLUMN mrr_range text;
+ALTER TABLE dmart.list_subscription_mrr ADD COLUMN mrr_range_id int;
+
+UPDATE dmart.list_subscription_mrr
+SET mrr_range = (SELECT mrr_range_hlp FROM temp_mrr_range WHERE mrr < higher_mrr_range and mrr >= lower_mrr_range);
+
+UPDATE dmart.list_subscription_mrr
+SET mrr_range_id = (SELECT id FROM temp_mrr_range WHERE mrr_range = mrr_range_hlp);
+
+INSERT INTO dmart.list_subscription_mrr (id, mrr, mrr_range_id) VALUES (-1, 0, -1);
+
+CREATE INDEX list_subscription_mrr_idx
+  ON dmart.list_subscription_mrr
+  USING btree
+  (id);
+
+DROP TABLE IF EXISTS dmart.list_last_churn_case_per_account;
+CREATE TABLE dmart.list_last_churn_case_per_account AS (SELECT churn_cases.accountid as id, 
+	count(distinct churn_cases.id) as num_churn_cases, 
+	max(churn_cases.createddate) as latest_churn_case_created, 
+	tmptble.status as latest_churn_case_status
+	from report.dmart_churn_cases churn_cases 
+	INNER JOIN (select accountid, status, row_number() over (partition by accountid order by createddate desc) as rnk from report.dmart_churn_cases) as tmptble 
+	ON tmptble.accountid = churn_cases.accountid
+	where rnk = 1
+	group by churn_cases.accountid, tmptble.status
+	order by churn_cases.accountid);
+
+INSERT INTO dmart.list_last_churn_case_per_account (id, num_churn_cases, latest_churn_case_created, latest_churn_case_status) VALUES (-1, 0, '2009-01-01', 'N/A');
+
+CREATE INDEX list_churn_case_per_account_idx
+  ON dmart.list_last_churn_case_per_account
+  USING btree
+  (id);
+
+DROP TABLE IF EXISTS dmart.list_last_success_case_per_account;
+CREATE TABLE dmart.list_last_success_case_per_account AS (SELECT success_cases.accountid as id, 
+	count(distinct success_cases.id) as num_success_cases, 
+	max(success_cases.createddate) as latest_success_case_created, 
+	tmptble.status as latest_success_case_status
+	from report.dmart_success_cases success_cases 
+	INNER JOIN (select accountid, status, row_number() over (partition by accountid order by createddate desc) as rnk from report.dmart_success_cases) as tmptble 
+	ON tmptble.accountid = success_cases.accountid
+	where rnk = 1
+	group by success_cases.accountid, tmptble.status
+	order by success_cases.accountid);
+
+INSERT INTO dmart.list_last_success_case_per_account (id, num_success_cases, latest_success_case_created, latest_success_case_status) VALUES (-1, 0, '2009-01-01', 'N/A');
+
+CREATE INDEX list_success_case_per_account_idx
+  ON dmart.list_last_success_case_per_account
+  USING btree
+  (id);
+
 /**
 Create SFDC Account List Table which serves as the basis for Salesforce data
 */
 DROP TABLE IF EXISTS dmart.list_sfdc_accounts;
 CREATE TABLE dmart.list_sfdc_accounts AS (
-	SELECT Account.Id, Account.Name, Account.dShopId__c::integer as merchant_profile_id, Account.strMerchantUUID__c as merchant_uuid, 
+	SELECT Account.Id, Account.Name, 
+	Account.dShopId__c::integer as merchant_profile_id, 
+	Account.strMerchantUUID__c as merchant_uuid, 
 	Account.dKeyAccountId__c::integer as merchant_key_account_id, 
-	Account.dTotalActivityScore__c as activity_score, 
+	COALESCE(users.name, 'N/A') as account_owner,
+	COALESCE(Account.merchant_happiness__c, 'N/A') as merchant_happiness,
+	COALESCE(Account.dTotalActivityScore__c, 0)::text as activity_score, 
 	ONB2__Subscription__c.lastmodifieddate,
 	ONB2__Subscription__c.ONB2__startdate__c,
+	ONB2__Subscription__c.createddate,
+	COALESCE(list_subscription_mrr.mrr, 0) as mrr,
+	COALESCE(list_subscription_mrr.mrr_range, 'N/A') as mrr_range,
+	COALESCE(list_subscription_mrr.mrr_range_id, -1) as mrr_range_id,
 	list_city.id as city_id,
 	list_industry.id as industry_id,
 	list_subscription_status.id as subscription_status_id,
 	list_subscription_channel.id as subscription_channel_id,
 	list_subscription_start_date.created_at_id as subscription_start_id,
-	list_subscription_end_date.created_at_id as subscription_end_id
+	list_subscription_end_date.created_at_id as subscription_end_id,
+	COALESCE(list_last_success_case_per_account.num_success_cases, 0) as num_success_cases,
+	COALESCE(list_last_success_case_per_account.latest_success_case_created, '2009-01-01')::date as latest_success_case_created,
+	COALESCE(list_last_success_case_per_account.latest_success_case_status, 'N/A') as latest_success_case_status,
+	COALESCE(list_last_churn_case_per_account.num_churn_cases, 0) as num_churn_cases,
+	COALESCE(list_last_churn_case_per_account.latest_churn_case_created, '2009-01-01')::date as latest_churn_case_created,
+	COALESCE(list_last_churn_case_per_account.latest_churn_case_status, 'N/A') as latest_churn_case_status
 	FROM prod.Account
+	LEFT JOIN prod.users
+	ON account.ownerid = users.id
+	LEFT JOIN dmart.list_subscription_mrr 
+	on list_subscription_mrr.id = account.refActiveSubscription__c
 	LEFT JOIN dmart.list_industry
 	ON list_industry.Industry = Account.Industry
 	LEFT JOIN dmart.list_subscription_status
@@ -233,9 +341,22 @@ CREATE TABLE dmart.list_sfdc_accounts AS (
 	ON list_subscription_start_date.date = ONB2__Subscription__c.ONB2__StartDate__c
 	LEFT JOIN dmart.list_subscription_end_date
 	ON list_subscription_end_date.date = ONB2__Subscription__c.ONB2__EndDate__c
+	LEFT JOIN dmart.list_last_success_case_per_account
+	ON list_last_success_case_per_account.id = account.id
+	LEFT JOIN dmart.list_last_churn_case_per_account
+	ON list_last_churn_case_per_account.id = account.id
 	WHERE dshopId__c NOTNULL
 	AND refActiveSubscription__c NOTNULL
 );
+
+UPDATE dmart.list_sfdc_accounts
+SET mrr_range = CASE WHEN mrr >= 300 THEN '>= 300€' ELSE mrr_range END;
+
+UPDATE dmart.list_sfdc_accounts
+SET mrr_range_id = CASE WHEN mrr > 300 THEN (SELECT id FROM temp_mrr_range WHERE lower_mrr_range >= 300 and higher_mrr_range <= 350) ELSE mrr_range_id END;
+
+
+ALTER TABLE dmart.list_sfdc_accounts ALTER COLUMN mrr TYPE text;
 
 /**
 The following part is used to get rid of duplicate merchant profile ids which lead to a cross join in creating the fact table
@@ -274,7 +395,25 @@ where rnk > 1
 ;
 delete from dmart.list_sfdc_accounts where list_sfdc_accounts.merchant_profile_id in (select dup_records.id from dup_records) and list_sfdc_accounts.ONB2__startdate__c in(select dup_records.ONB2__startdate__c from dup_records);
 
+drop table if exists to_del;
+create temp table to_del as select distinct merchant_profile_id as id, createddate from dmart.list_sfdc_accounts;
+drop table if exists dup_records;
+create temp table dup_records as 
+select * 
+from (
+   select id, 
+          createddate, 
+          row_number() over (partition by id order by createddate desc) as rnk
+   from to_del
+) as t
+where rnk > 1
+;
+delete from dmart.list_sfdc_accounts where list_sfdc_accounts.merchant_profile_id in (select dup_records.id from dup_records) and list_sfdc_accounts.createddate in(select dup_records.createddate from dup_records);
+
+
 INSERT INTO dmart.list_sfdc_accounts (merchant_profile_id, id, Name) VALUES (-1, 'N/A', 'N/A');
+
+
 
 -- Index is created
 CREATE INDEX list_sfdc_accounts_idx
@@ -331,6 +470,20 @@ CREATE TEMP TABLE temp_sma AS (
 	GROUP BY date_id, merchant_profile_id
 );
 
+DROP TABLE IF EXISTS temp_mobile_sma;
+CREATE TEMP TABLE temp_mobile_sma AS (
+	SELECT list_date_02.created_at_id as date_id, 
+	appointment_series.merchant_profile_id,
+	count(distinct appointment_series.id) as mobile_sma
+	FROM prod.appointment_series
+	INNER JOIN dmart.list_date_02
+	ON list_date_02.date = date_trunc('day', appointment_series.created_at)::date
+	WHERE appointment_series.origin LIKE '%mobile%' 
+	AND (appointment_series.origin LIKE '%dienstleister%' 
+		OR appointment_series.origin LIKE '%merchant%')
+	GROUP BY date_id, merchant_profile_id
+);
+
 DROP TABLE IF EXISTS temp_bookings;
 CREATE TEMP TABLE temp_bookings AS (
 	SELECT list_date_02.created_at_id as date_id, 
@@ -340,6 +493,20 @@ CREATE TEMP TABLE temp_bookings AS (
 	INNER JOIN dmart.list_date_02
 	ON list_date_02.date = date_trunc('day', appointment_series.created_at)::date
 	WHERE appointment_series.created_by_merchant = false
+	GROUP BY date_id, merchant_profile_id
+);
+
+DROP TABLE IF EXISTS temp_mobile_bookings;
+CREATE TEMP TABLE temp_mobile_bookings AS (
+	SELECT list_date_02.created_at_id as date_id, 
+	appointment_series.merchant_profile_id,
+	count(distinct appointment_series.id) as mobile_bookings
+	FROM prod.appointment_series
+	INNER JOIN dmart.list_date_02
+	ON list_date_02.date = date_trunc('day', appointment_series.created_at)::date
+	WHERE appointment_series.origin LIKE '%mobile%' 
+	AND (appointment_series.origin LIKE '%kunde%' 
+		OR appointment_series.origin LIKE '%customer%')
 	GROUP BY date_id, merchant_profile_id
 );
 
@@ -508,7 +675,9 @@ COALESCE(list_sfdc_accounts.subscription_end_id, -1) as subscription_end_id,
 COALESCE(agg_resources.resource_count, 0) as number_resources,
 COALESCE(agg_services.service_count, 0) as number_services,
 COALESCE(temp_bookings.bookings, 0) as bookings,
+COALESCE(temp_mobile_bookings.mobile_bookings, 0) as mobile_bookings,
 COALESCE(temp_sma.sma, 0) as sma,
+COALESCE(temp_mobile_sma.mobile_sma, 0) as mobile_sma,
 COALESCE(temp_feedbacks.feedbacks, 0) as feedbacks,
 COALESCE(temp_messages.messages, 0) as messages,
 COALESCE(temp_messages.conversations, 0) as conversations, 
@@ -531,9 +700,15 @@ ON agg_services.merchant_profile_id = temp_fact_growth01_basis.merchant_profile_
 LEFT JOIN temp_bookings
 ON temp_bookings.merchant_profile_id = temp_fact_growth01_basis.merchant_profile_id 
 AND temp_bookings.date_id = temp_fact_growth01_basis.date_id
+LEFT JOIN temp_mobile_bookings
+ON temp_mobile_bookings.merchant_profile_id = temp_fact_growth01_basis.merchant_profile_id 
+AND temp_mobile_bookings.date_id = temp_fact_growth01_basis.date_id
 LEFT JOIN temp_sma
 ON temp_sma.merchant_profile_id = temp_fact_growth01_basis.merchant_profile_id 
 AND temp_sma.date_id = temp_fact_growth01_basis.date_id
+LEFT JOIN temp_mobile_sma
+ON temp_mobile_sma.merchant_profile_id = temp_fact_growth01_basis.merchant_profile_id 
+AND temp_mobile_sma.date_id = temp_fact_growth01_basis.date_id
 LEFT JOIN temp_feedbacks
 ON temp_feedbacks.merchant_profile_id = temp_fact_growth01_basis.merchant_profile_id 
 AND temp_feedbacks.date_id = temp_fact_growth01_basis.date_id
@@ -592,18 +767,20 @@ CREATE INDEX fact_growth_general_subscription_channel_id_idx
 
 DELETE FROM dmart.fact_growth_general WHERE bookings = 0 
 AND sma = 0 
+AND mobile_sma = 0
+AND mobile_bookings = 0
 AND feedbacks = 0
-and messages = 0
-and conversations  = 0
-and shifts = 0
-and newsletters = 0
-and newsletter_recipients = 0
-and booking_widget_payments = 0
-and booking_widget_payment_amount = 0
-and gastrofix_payments = 0
-and gastrofix_payment_amount = 0
-and emails_sent = 0
-and sms_sent = 0;
+AND messages = 0
+AND conversations  = 0
+AND shifts = 0
+AND newsletters = 0
+AND newsletter_recipients = 0
+AND booking_widget_payments = 0
+AND booking_widget_payment_amount = 0
+AND gastrofix_payments = 0
+AND gastrofix_payment_amount = 0
+AND emails_sent = 0
+AND sms_sent = 0;
 
 
 insert into dmart.list_subscription_start_date (created_at_id, date, full_day_description, day_of_week, calendar_month, calendar_year, fiscal_year_month, holiday, weekend,month_id, order_day_of_week)
@@ -612,4 +789,9 @@ values(-1, '2009-01-01','N/A','N/A','N/A',9999,'N/A','N/A','N/A', 0, 9999);
 insert into dmart.list_subscription_end_date (created_at_id, date, full_day_description, day_of_week, calendar_month, calendar_year, fiscal_year_month, holiday, weekend,month_id, order_day_of_week)
 values(-1, '2009-01-01','N/A','N/A','N/A',9999,'N/A','N/A','N/A', 0, 9999);
 
+alter table dmart.list_sfdc_accounts alter column latest_churn_case_created TYPE text;
+alter table dmart.list_sfdc_accounts alter column latest_success_case_created TYPE text;
+alter table dmart.list_sfdc_accounts alter column num_success_cases TYPE text;
+alter table dmart.list_sfdc_accounts alter column num_churn_cases TYPE text;
 
+--select * from dmart.list_sfdc_accounts where num_churn_cases is null
---
+++ jira/jira_python.py
@@ -0,0 +1,80 @@
+from sqlalchemy import create_engine
+from threading import Thread, Lock
+import pandas as pd
+import threading
+import psycopg2
+import os
+import jira.client
+from jira.client import JIRA
+import etl_delta_load as etl
+import sys
+
+reload(sys)
+sys.setdefaultencoding('utf8')
+
+conn_string = "host='segment-warehouse.c9sl8obym0ce.eu-central-1.rds.amazonaws.com' dbname='business-intelligence' user='segment' password='Aa123456_'"
+conn = psycopg2.connect(conn_string)
+curs = conn.cursor()
+
+options = {'server': 'https://jira.shore.com'}
+jira = JIRA(options, basic_auth=('sanjiv.upadhyaya@shore.com', 'AndreJuliaSanjiv@shore'))
+projects = jira.projects()
+#issues = jira.search_issues("updated >= -2w  order by created DESC", startAt=0, maxResults=20000, validate_query=True, fields=None, expand='changelog,customfield', json_result=None)
+issues = jira.search_issues("updated >= -2d and updated < -1d  order by created DESC", startAt=0, maxResults=2000, validate_query=True, fields=None, expand='changelog,customfield', json_result=None)
+
+for i in projects:
+        curs.execute("insert into jira.map_issues(project_id, project_name,etl_date) values('{0}','{1}',current_date)".format(i.key,i.name))
+        conn.commit()
+
+# for issues and to track their history in life time
+for i in issues:
+        issue = jira.issue(i)
+        changelog = issue.changelog
+        for history in changelog.histories:
+                for item in history.items:
+                        print item.field
+                        if item.field == 'status':
+                                print i.key,issue.fields.status,issue.fields.timeestimate, issue.fields.description,issue.fields.summary, issue.fields.priority , issue.fields.assignee, issue.fields.status, issue.fields.labels, issue.fields.created, issue.fields.updated,issue.fields.issuetype, issue.fields.creator
+                                curs.execute("insert into jira.issues_history (timeestimate,description,status_changed,history,summary, priority, assignee, status, labels, created, updated, issuetype, creator, issue_id, etl_date) values ('{0}','{1}','{2}','{3}','{4}','{5}','{6}','{7}','{8}','{9}'::timestamp,'{10}'::timestamp,'{11}','{12}','{13}', current_date)"
+                                .format(issue.fields.timeestimate,issue.fields.description.replace("'","") if issue.fields.description   else issue.fields.description,history.created , ' From:' + item.fromString + ' To:' + item.toString,str(issue.fields.summary).replace("'",""), issue.fields.priority , issue.fields.assignee, issue.fields.status, str(issue.fields.labels).replace("'",""), issue.fields.created, issue.fields.updated,issue.fields.issuetype, issue.fields.creator, i.key))
+                                curs.execute("update jira.issues set project_id ={0}".format("substring(issue_id,1,position('-' in issue_id)-1)"))
+                                conn.commit()
+
+# For issues which are still in the backlog
+for i in issues:
+        issue = jira.issue(i)
+        curs.execute("insert into jira.issues (timeestimate,description,summary, priority, assignee, status, labels, created, updated, issuetype, creator, issue_id, etl_date) values ('{0}','{1}','{2}','{3}','{4}','{5}','{6}','{7}'::timestamp,'{8}'::timestamp,'{9}','{10}','{11}', current_date)"
+        .format(issue.fields.timeestimate,issue.fields.description.replace("'","") if issue.fields.description   else issue.fields.description,str(issue.fields.summary).replace("'",""), issue.fields.priority , issue.fields.assignee, issue.fields.status, str(issue.fields.labels).replace("'",""), issue.fields.created, issue.fields.updated,issue.fields.issuetype, issue.fields.creator, i.key))
+        curs.execute("update jira.issues set project_id ={0}".format("substring(issue_id,1,position('-' in issue_id)-1)"))
+        conn.commit()
+
+# story point
+for i in issues:
+        try:
+                curs.execute("insert into jira.issue_story_points(issue_id, points, etl_date) values('{}','{}', current_date)".format(i,i.fields.customfield_10006))
+                conn.commit()
+        except AttributeError:
+                curs.execute("insert into jira.issue_story_points(issue_id, points, etl_date) values('{}','{}', current_date)".format(i,'N/A'))
+                conn.commit()
+
+# sprint information
+for i in issues:
+        if i.fields.customfield_10001:
+                for item in i.fields.customfield_10001:
+                        try:
+                                print item, i
+                                curs.execute("insert into jira.sprints(issue_id, sprint_info, etl_date) values ('{0}','{1}',current_date)".format(str(i), item))
+                                conn.commit()
+                        except AttributeError:
+                                pass
+        else:
+                curs.execute("insert into jira.sprints(issue_id, sprint_info, etl_date) values ('{0}','{1}', current_date)".format(str(i), 'N/A'))
+                conn.commit()
+
+# run the required transformation after the importing all data
+for keys in etl.query:
+        curs.execute(etl.query[keys])
+        conn.commit()
+
+conn.close()
+curs.close()
