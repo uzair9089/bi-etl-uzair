74cc0ebdf51e29f022151c1cb1f6ee3efe592d7a
Sanjiv Upadhyaya <sanjivupadhyaya@Sanjivs-MacBookPro-2.local>
2017-03-16 14:37:05 +0100

    adding cron and log files for the jira

---
+++ jira/crons/cron_jira.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+source /opt/credential/pass.sh && python /opt/etl/jira/jira_python.py  >> /tmp/log_jira$$.log 2>&1
+mailx -s "ETL Status - Jira" sanjiv.upadhyaya@shore.com < /tmp/log_jira$$.log
+cat /tmp/log_jira$$.log >> /opt/etl/jira/logs/log_jira.txt
+rm /tmp/log_jira$$.log
