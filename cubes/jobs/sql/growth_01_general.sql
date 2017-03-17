c9736fa8d01a5f898dbd9f6c40b1b9143438f8fd
Sanjiv Upadhyaya <sanjivupadhyaya@Sanjivs-MacBookPro-2.local>
2017-03-17 10:33:24 +0100

    modified the script to take care of the data types and the null values

--- cubes/jobs/sql/growth_01_general.sql
+++ cubes/jobs/sql/growth_01_general.sql
@@ -795,4 +795,6 @@ alter table dmart.list_sfdc_accounts alter column num_success_cases TYPE text;
 alter table dmart.list_sfdc_accounts alter column num_churn_cases TYPE text;
 alter table dmart.list_date_02 add column week int;
 update dmart.list_date_02 set week = extract(week from  date );
+delete from dmart.list_sfdc_accounts where num_churn_cases is null or num_success_cases is null;
 --select * from dmart.list_sfdc_accounts where num_churn_cases is null
+
