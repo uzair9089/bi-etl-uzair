6f0c32b9dfc32e25c75fc0a6fc609aa433c1d944
Sanjiv Upadhyaya <sanjivupadhyaya@Sanjivs-MacBookPro-2.local>
2017-03-20 11:36:13 +0100

    removed the line prints from the jira_python

--- jira/jira_python.py
+++ jira/jira_python.py
@@ -35,9 +35,9 @@ for i in issues:
         changelog = issue.changelog
         for history in changelog.histories:
                 for item in history.items:
-                        print item.field
+                        #print item.field
                         if item.field == 'status':
-                                print i.key,issue.fields.status,issue.fields.timeestimate, issue.fields.description,issue.fields.summary, issue.fields.priority , issue.fields.assignee, issue.fields.status, issue.fields.labels, issue.fields.created, issue.fields.updated,issue.fields.issuetype, issue.fields.creator
+                                #print i.key,issue.fields.status,issue.fields.timeestimate, issue.fields.description,issue.fields.summary, issue.fields.priority , issue.fields.assignee, issue.fields.status, issue.fields.labels, issue.fields.created, issue.fields.updated,issue.fields.issuetype, issue.fields.creator
                                 curs.execute("insert into jira.issues_history (timeestimate,description,status_changed,history,summary, priority, assignee, status, labels, created, updated, issuetype, creator, issue_id, etl_date) values ('{0}','{1}','{2}','{3}','{4}','{5}','{6}','{7}','{8}','{9}'::timestamp,'{10}'::timestamp,'{11}','{12}','{13}', current_date)"
                                 .format(issue.fields.timeestimate,issue.fields.description.replace("'","") if issue.fields.description   else issue.fields.description,history.created , ' From:' + item.fromString + ' To:' + item.toString,str(issue.fields.summary).replace("'",""), issue.fields.priority , issue.fields.assignee, issue.fields.status, str(issue.fields.labels).replace("'",""), issue.fields.created, issue.fields.updated,issue.fields.issuetype, issue.fields.creator, i.key))
                                 curs.execute("update jira.issues set project_id ={0}".format("substring(issue_id,1,position('-' in issue_id)-1)"))
@@ -65,7 +65,7 @@ for i in issues:
         if i.fields.customfield_10001:
                 for item in i.fields.customfield_10001:
                         try:
-                                print item, i
+                                #print item, i
                                 curs.execute("insert into jira.sprints(issue_id, sprint_info, etl_date) values ('{0}','{1}',current_date)".format(str(i), item))
                                 conn.commit()
                         except AttributeError:
