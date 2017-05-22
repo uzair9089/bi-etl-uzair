28a34a9a5071bd94d56865960159307dae15a638
Sanjiv Upadhyaya <sanjivupadhyaya@shore.com>
2017-05-22 12:32:49 +0200

    testing

--- redshift/importer.py
+++ redshift/importer.py
@@ -79,10 +79,10 @@ class Importer(Thread):
             print self.full_path
 
             for i in bucket:
-                print (""" COPY %s FROM 's3://shore-bi-etl'%s iam_role 'arn:aws:iam::601812874785:role/BIs3Access' CSV IGNOREHEADER 1 """ % (self.file_name,i.key))
+                #print (""" COPY %s FROM 's3://shore-bi-etl%s' iam_role 'arn:aws:iam::601812874785:role/BIs3Access' CSV IGNOREHEADER 1 """ % (self.file_name,i.key))
                 print i.key
                 if self.full_path == i.key:
-                    print (""" COPY %s FROM 's3://shore-bi-etl'%s iam_role 'arn:aws:iam::601812874785:role/BIs3Access' CSV IGNOREHEADER 1 """ % (self.file_name,i.key))
+                    print (""" COPY %s FROM 's3://shore-bi-etl%s' iam_role 'arn:aws:iam::601812874785:role/BIs3Access' CSV IGNOREHEADER 1 """ % (self.file_name,i.key))
                    # print 'finally insertingggg wohuu'
                    # print i
 
