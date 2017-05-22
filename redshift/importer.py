b0052db6d6aab6e082da32a3b80773033161576d
Sanjiv Upadhyaya <sanjivupadhyaya@shore.com>
2017-05-22 12:20:26 +0200

    testing

--- redshift/importer.py
+++ redshift/importer.py
@@ -71,7 +71,7 @@ class Importer(Thread):
 
             #print (""" COPY %s FROM 's3://shore-bi-etl'%s iam_role 'arn:aws:iam::601812874785:role/BIs3Access' CSV IGNOREHEADER 1 """ % (self.file_name,bucket.lookup(param.full_path+self.file_name+'.csv')))
 
-            if bucket.lookup(param.full_path+self.file_name+'.csv'):
+            #if bucket.lookup(param.full_path+self.file_name+'.csv'):
               #  print 'file exists'
                # print (""" COPY %s FROM 's3://shore-bi-etl'%s iam_role 'arn:aws:iam::601812874785:role/BIs3Access' CSV IGNOREHEADER 1 """ % (self.file_name,bucket.lookup(param.full_path+self.file_name+'.csv')))
                     #self.full_path))
