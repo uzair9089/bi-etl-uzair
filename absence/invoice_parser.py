68a15aa674fbdee6e4b7272cffe33e07799f0807
Sanjiv Upadhyaya <sanjivupadhyaya@shore.com>
2017-05-23 12:21:23 +0200

    finalized import of invoices from absence

--- absence/invoice_parser.py
+++ absence/invoice_parser.py
@@ -29,7 +29,7 @@ class InvoiceParser:
       for line in file:
         x = json.loads(line,strict=False) 
         keys_in_record = x.keys() 
-        print keys_in_record
+        #print keys_in_record
 
         if 'data' in keys_in_record:
 
@@ -40,64 +40,60 @@ class InvoiceParser:
             
             if 'invoiceId' in data_lines.keys():
               invoice_id = str(data_lines["invoiceId"]).replace("'","")
-            print invoice_id
+              #print invoice_id
 
             if 'company' in data_lines.keys():
               company = data_lines["company"]
-              print company
+              #print company
 
             if 'invoiceBatch' in data_lines.keys():
               invoice_batch = data_lines["invoiceBatch"]
-              print invoice_batch
+              #print invoice_batch
 
             if 'invoiceMonth' in data_lines.keys(): 
               invoice_month = str(data_lines["invoiceMonth"]).replace("'","")
-              print invoice_month
+              #print invoice_month
 
             if 'billDate' in data_lines.keys():
               bill_date = str(data_lines["billDate"]).replace("'","")
               #email.replace("'","")
-              print bill_date
+              #print bill_date
 
             if 'items' in data_lines.keys():
               items = str(data_lines["items"]).replace("'","")
-              print items
+              #print items
 
             if '_id' in data_lines.keys():
               _id = str(data_lines["_id"]).replace("'","")
-              print _id
+              #print _id
 
             if 'billingInterval' in data_lines.keys():
               billing_interval = data_lines["billingInterval"]
-              print billing_interval
+              #print billing_interval
 
             if 'pricingType' in data_lines.keys():
               pricing_type = str(data_lines["pricingType"]).replace("'","")
-              print pricing_type
+              #print pricing_type
 
             if 'priceGross' in data_lines.keys():
               price_gross = str(data_lines["priceGross"]).replace("'","")
-              print price_gross
+              #print price_gross
 
             if 'invoiceCounter' in data_lines.keys():
               invoice_counter = str(data_lines["invoiceCounter"]).replace("'","")
-              print invoice_counter
+              #print invoice_counter
 
             if 'priceNet' in data_lines.keys():
               price_net = str(data_lines["priceNet"]).replace("'","")
-              print price_net
+              #print price_net
 
             if 'invoiceStatus' in data_lines.keys():
               invoice_status = str(data_lines["invoiceStatus"]).replace("'","")
-              print invoice_status
+              #print invoice_status
 
             curs.execute("insert into absence.invoices(invoice_id, company, invoice_batch, invoice_month, bill_date, items, _id, billing_interval, payment_type, pricing_type, price_gross, invoice_counter, price_net, invoice_status, vat) values ('{0}','{1}','{2}','{3}','{4}','{5}','{6}',{7},{8},{9},{10},{11},{12},{13},{14})"
             .format(invoice_id, company, invoice_batch, invoice_month, bill_date, items, _id, billing_interval, payment_type, pricing_type, price_gross, invoice_counter, price_net, invoice_status, vat))
             conn.commit()
-
-            #print ("insert into absence.invoices(invoice_id, company, invoice_batch, invoice_month, bill_date, items, _id, billing_interval, payment_type, pricing_type, price_gross, invoice_counter, price_net, invoice_status, vat) values ('{0}','{1}','{2}','{3}','{4}','{5}','{6}',{7},{8},{9},{10},{11},{12},{13},{14})"
-            #.format(invoice_id, company, invoice_batch, invoice_month, bill_date, items, _id, billing_interval, payment_type, pricing_type, price_gross, invoice_counter, price_net, invoice_status, vat))
-            #print ("here after commit")
 
 
       print("finished parsing data for: " + collection_name)
