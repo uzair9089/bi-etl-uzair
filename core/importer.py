a1f6b242b61f7d56c6aab10418c9a351a7d71970
Sanjiv Upadhyaya <sanjivupadhyaya@shore.com>
2017-06-20 17:23:34 +0200

    changes for nwsl

--- core/importer.py
+++ core/importer.py
@@ -48,7 +48,7 @@ class Importer(Thread):
                 file = open(param.newpath +self.file_name)
 
                 if self.file_name == 'newsletters_deprecated.csv':
-                    curs.copy_expert(sql = """ COPY %s FROM STDIN WITH CSV HEADER DELIMITER AS ',' ENCODING 'latin1' """ % (param.schema +'.' +self.file_name[:-4]), file = file)
+                    curs.copy_expert(sql = """ COPY %s FROM STDIN WITH CSV HEADER DELIMITER AS ',', ENCODING 'ISO_8859_5' """ % (param.schema +'.' +self.file_name[:-4]), file = file)
                     conn.commit()
                 else:
                     curs.copy_expert(sql = """ COPY %s FROM STDIN WITH CSV HEADER DELIMITER AS ',' """ % (param.schema +'.' +self.file_name[:-4]), file = file)
