a1d03d1cc1edde38cdba3dd77ae2138fd7377d29
Sanjiv Upadhyaya <sanjivupadhyaya@shore.com>
2017-06-20 12:28:24 +0200

    re-run of nwsl

--- core/etl_delta_load.py
+++ core/etl_delta_load.py
@@ -12,6 +12,8 @@ from param import param
 truncate_queries = {
 					# newsletters
 					'newsletter_customers_deprecated': "truncate table nwsl.newsletter_customers_deprecated;"
+					,'newsletters_deprecated': "truncate table nwsl.newsletters_deprecated;"
+
 					# core
 					,'employees': "truncate table core.employees;"
 					,'merchant_key_accounts':"truncate table core.merchant_key_accounts;"
@@ -44,8 +46,8 @@ delta_query = {
 					# newsletters
 			'newsletters_nwsl': "drop table if exists newsletters_nwsl_id; create temp table newsletters_nwsl_id as select distinct id from nwsl.newsletters_nwsl where updated_at>='" +str(param.start_date) +"' and  updated_at<'" +str(param.end_date) +"';delete from stage.s_newsletters_nwsl where id in (select id from newsletters_nwsl_id); insert into stage.s_newsletters_nwsl select * from (select *,row_number() over (partition by id order by updated_at desc) as rnk from nwsl.newsletters_nwsl where updated_at>='" +str(param.start_date) +"' and  updated_at<'" +str(param.end_date) +"') as t where rnk=1;"
 			,'newsletter_customers_deprecated': "truncate table stage.s_newsletter_customers_deprecated; insert into stage.s_newsletter_customers_deprecated select * from nwsl.newsletter_customers_deprecated;"
-
-			,'newsletters_deprecated': "drop table if exists newsletters_deprecated_id; create temp table newsletters_deprecated_id as select distinct id from nwsl.newsletters_deprecated where updated_at>='" +str(param.start_date) +"' and  updated_at<'" +str(param.end_date) +"';delete from stage.s_newsletters_deprecated where id in (select id from newsletters_deprecated_id); insert into stage.s_newsletters_deprecated select * from (select *,row_number() over (partition by id order by updated_at desc) as rnk from nwsl.newsletters_deprecated where updated_at>='" +str(param.start_date) +"' and  updated_at<'" +str(param.end_date) +"') as t where rnk=1;"
+			,'newsletters_deprecated' : "truncate table stage.s_newsletters_deprecated; insert into stage.s_newsletters_deprecated select * from nwsl.newsletters_deprecated;"
+			#,'newsletters_deprecated': "drop table if exists newsletters_deprecated_id; create temp table newsletters_deprecated_id as select distinct id from nwsl.newsletters_deprecated where updated_at>='" +str(param.start_date) +"' and  updated_at<'" +str(param.end_date) +"';delete from stage.s_newsletters_deprecated where id in (select id from newsletters_deprecated_id); insert into stage.s_newsletters_deprecated select * from (select *,row_number() over (partition by id order by updated_at desc) as rnk from nwsl.newsletters_deprecated where updated_at>='" +str(param.start_date) +"' and  updated_at<'" +str(param.end_date) +"') as t where rnk=1;"
 
 			# messages
 			,'conversations': "drop table if exists conversations_id; create temp table conversations_id as select distinct id from msg.conversations where updated_at>='" +str(param.start_date) +"' and  updated_at<'" +str(param.end_date) +"';delete from stage.s_conversations where id in (select id from conversations_id); insert into stage.s_conversations select * from (select *,row_number() over (partition by id order by updated_at desc) as rnk from msg.conversations where updated_at>='" +str(param.start_date) +"' and  updated_at<'" +str(param.end_date) +"') as t where rnk=1;"
@@ -152,7 +154,8 @@ delete_queries = {
 
 				# newsletter
 				,'newsletters_nwsl': "delete from nwsl.newsletters_nwsl where updated_at>='" +str(param.reset_start_date) +"';"
-				,'newsletters_deprecated': "drop table if exists newsletters_deprecated_id; create temp table newsletters_deprecated_id as select distinct id from nwsl.newsletters_deprecated where updated_at>='" +str(param.start_date) +"' and  updated_at<'" +str(param.end_date) +"';delete from stage.s_newsletters_deprecated where id in (select id from newsletters_deprecated_id); insert into stage.s_newsletters_deprecated select * from (select *,row_number() over (partition by id order by updated_at desc) as rnk from nwsl.newsletters_deprecated where updated_at>='" +str(param.start_date) +"' and  updated_at<'" +str(param.end_date) +"') as t where rnk=1;"
+				#,'newsletters_deprecated': "drop table if exists newsletters_deprecated_id; create temp table newsletters_deprecated_id as select distinct id from nwsl.newsletters_deprecated where updated_at>='" +str(param.start_date) +"' and  updated_at<'" +str(param.end_date) +"';delete from stage.s_newsletters_deprecated where id in (select id from newsletters_deprecated_id); insert into stage.s_newsletters_deprecated select * from (select *,row_number() over (partition by id order by updated_at desc) as rnk from nwsl.newsletters_deprecated where updated_at>='" +str(param.start_date) +"' and  updated_at<'" +str(param.end_date) +"') as t where rnk=1;"
+				#,'newsletters_deprecated' : "truncate table stage.s_newsletters_deprecated; insert into stge.s_newsletters_deprecated select * from nwsl.newsletters_deprecated;"
 
 
 				# messages
@@ -184,7 +187,8 @@ delta_query_reset = {
 				# newsletters
 				'newsletters_nwsl': "drop table if exists newsletters_nwsl_id; create temp table newsletters_nwsl_id as select distinct id from nwsl.newsletters_nwsl where updated_at>='" +str(param.reset_start_date) +"' and  updated_at<'" +str(param.reset_end_date) +"';delete from stage.s_newsletters_nwsl where id in (select id from newsletters_nwsl_id); insert into stage.s_newsletters_nwsl select * from (select *,row_number() over (partition by id order by updated_at desc) as rnk from nwsl.newsletters_nwsl where updated_at>='" +str(param.reset_start_date) +"' and  updated_at<'" +str(param.reset_end_date) +"') as t where rnk=1;"
 				,'newsletter_customers_deprecated': "truncate table stage.s_newsletter_customers_deprecated; insert into stage.s_newsletter_customers_deprecated select * from nwsl.newsletter_customers_deprecated;"
-				,'newsletters_deprecated': "drop table if exists newsletters_deprecated_id; create temp table newsletters_deprecated_id as select distinct id from nwsl.newsletters_deprecated where updated_at>='" +str(param.start_date) +"' and  updated_at<'" +str(param.end_date) +"';delete from stage.s_newsletters_deprecated where id in (select id from newsletters_deprecated_id); insert into stage.s_newsletters_deprecated select * from (select *,row_number() over (partition by id order by updated_at desc) as rnk from nwsl.newsletters_deprecated where updated_at>='" +str(param.start_date) +"' and  updated_at<'" +str(param.end_date) +"') as t where rnk=1;"
+				#,'newsletters_deprecated': "drop table if exists newsletters_deprecated_id; create temp table newsletters_deprecated_id as select distinct id from nwsl.newsletters_deprecated where updated_at>='" +str(param.start_date) +"' and  updated_at<'" +str(param.end_date) +"';delete from stage.s_newsletters_deprecated where id in (select id from newsletters_deprecated_id); insert into stage.s_newsletters_deprecated select * from (select *,row_number() over (partition by id order by updated_at desc) as rnk from nwsl.newsletters_deprecated where updated_at>='" +str(param.start_date) +"' and  updated_at<'" +str(param.end_date) +"') as t where rnk=1;"
+				,'newsletters_deprecated' : "truncate table stage.s_newsletters_deprecated; insert into stage.s_newsletters_deprecated select * from nwsl.newsletters_deprecated;"
 
 				# messages
 				,'conversations': "drop table if exists conversations_id; create temp table conversations_id as select distinct id from msg.conversations where updated_at>='" +str(param.reset_start_date) +"' and  updated_at<'" +str(param.reset_end_date) +"';delete from stage.s_conversations where id in (select id from conversations_id); insert into stage.s_conversations select * from (select *,row_number() over (partition by id order by updated_at desc) as rnk from msg.conversations where updated_at>='" +str(param.reset_start_date) +"' and  updated_at<'" +str(param.reset_end_date) +"') as t where rnk=1;"
