b8aadb72b13567b6c54c2e7f745b3716be2f4d30
Sanjiv Upadhyaya <sanjivupadhyaya@shore.com>
2017-06-20 10:50:11 +0200

    testing

--- redshift/importer.py
+++ redshift/importer.py
@@ -59,22 +59,24 @@ class Importer(Thread):
             identifier = self.file_name
 
             for i in bucket:
-                print '/'+i.key
+                #print '/'+i.key
                 if self.full_path == '/'+i.key:
                     #print 'entering the copy'
                     curs.execute (""" COPY %s.%s FROM 's3://shore-bi-etl/%s' iam_role 'arn:aws:iam::601812874785:role/BIs3Access' CSV IGNOREHEADER 1 """ % (param.schema, self.file_name,i.key))
+                    curs.execute(etl_delta_load.delta_query[self.file_name])
                     conn.commit()
+            print("delta load for: " +self.file_name +" completed ***")
 
                 #print("import for " +self.file_name +" completed !!!")
                 #print("delta load starts for:" +self.file_name)
 
-                if param.reset_time == param.reset_value:
-                    curs.execute(etl_delta_load.delta_query_reset[self.file_name])
-                    conn.commit()
-                    #print("delta load for: " +self.file_name +" completed ***RESET***")
-                else:
-                    curs.execute(etl_delta_load.delta_query[self.file_name])
-                    conn.commit()
+                    # if param.reset_time == param.reset_value:
+                    #     curs.execute(etl_delta_load.delta_query_reset[self.file_name])
+                    #     conn.commit()
+                    #     #print("delta load for: " +self.file_name +" completed ***RESET***")
+                    # else:
+                    #     curs.execute(etl_delta_load.delta_query[self.file_name])
+                    #     conn.commit()
                     #print("delta load for: " +self.file_name +" completed ***")
 
                 #print("import for " +self.file_name +" completed !!!")
