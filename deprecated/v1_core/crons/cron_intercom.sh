f56acb4e4b06f6c39ef6dfe602afd2c25643c49a
Sanjiv Upadhyaya <sanjivupadhyaya@shore.com>
2017-07-17 14:48:47 +0200

    testing

--- deprecated/v1_core/crons/cron_intercom.sh
+++ deprecated/v1_core/crons/cron_intercom.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-source /opt/credential/pass.sh && python /opt/etl/v1_core/runner.py intercom  >> /tmp/log_intercom$$.log 2>&1
+source /opt/credential/pass.sh && python /opt/etl/deprecated/v1_core/runner.py intercom  >> /tmp/log_intercom$$.log 2>&1
 mailx -s "ETL Status - Intercom" sanjiv.upadhyaya@shore.com < /tmp/log_intercom$$.log
 cat /tmp/log_intercom$$.log >> /opt/etl/v1_core/logs/log_intercom.txt
 rm /tmp/log_intercom$$.log
