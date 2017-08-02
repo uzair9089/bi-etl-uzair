a6f07afdd10fd1efa057ed4f519d6abf21e3745f
Andre <ag@shore.com>
2017-08-02 10:58:55 +0200

    recurly exporter update

--- recurly/exporter.py
+++ recurly/exporter.py
@@ -39,27 +39,32 @@ class Exporter(Thread):
 			else:
 				response = requests.request("GET", param.url[self.collection_name] + param.filters_new[self.collection_name], headers=param.headers, auth=HTTPBasicAuth(recurly_token, ''))
 
-			xml_string = ET.fromstring(response.content)
-			# set the exported_file to 1 so imported can start after csv is dumped
+			if response.headers['X-Records'] != '0':
 
-			if 'Link' in response.headers:
-				while 'rel="next"' in response.headers['Link']:
-					next_link = response.headers['Link']
-					#print(next_link)
-					if 'rel="start"' in next_link:
-						start_pos = next_link.find('rel="start"') + 14
-					else: 
-						start_pos = 1
-					end_pos = next_link.find('rel="next"') - 3
-					xml_link = next_link[start_pos:end_pos]
-					#print(xml_link)
-					response = requests.request('GET', xml_link, headers=param.headers, auth=HTTPBasicAuth(recurly_token, '')) # need to put that in environment variables
-					next_xml_string = ET.fromstring(response.content)
-					next_xml_string_child = next_xml_string.getchildren()
-					xml_string.extend(next_xml_string_child)
+				xml_string = ET.fromstring(response.content)
+				# set the exported_file to 1 so imported can start after csv is dumped
 
-			do = sp()
-			do.parser(xml_string)
+				if 'Link' in response.headers:
+					while 'rel="next"' in response.headers['Link']:
+						next_link = response.headers['Link']
+						#print(next_link)
+						if 'rel="start"' in next_link:
+							start_pos = next_link.find('rel="start"') + 14
+						else: 
+							start_pos = 1
+						end_pos = next_link.find('rel="next"') - 3
+						xml_link = next_link[start_pos:end_pos]
+						#print(xml_link)
+						response = requests.request('GET', xml_link, headers=param.headers, auth=HTTPBasicAuth(recurly_token, '')) # need to put that in environment variables
+						next_xml_string = ET.fromstring(response.content)
+						next_xml_string_child = next_xml_string.getchildren()
+						xml_string.extend(next_xml_string_child)
+
+				do = sp()
+				do.parser(xml_string)
+
+			else:
+				print('No records in response')
 
 		# catch the exception and put it into the etl_status if any
 		except Exception as e:
