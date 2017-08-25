7b0c7afb13690213e776e2afea48a6b55610e35a
Andre <ag@shore.com>
2017-08-25 16:45:30 +0200

    invoices parser missed id

--- absence/invoice_parser.py
+++ absence/invoice_parser.py
@@ -76,7 +76,7 @@ class InvoiceParser:
 					if 'invoiceStatus' in data_lines.keys():
 						invoice_status = str(data_lines["invoiceStatus"]).replace("'","")
 
-				curs.execute("insert into absence.invoices(invoice_id,  company, invoice_batch, invoice_month, bill_date, items, _id, billing_interval, payment_type, pricing_type, price_gross, invoice_counter, price_net, invoice_status, vat) values ('{0}','{1}','{2}','{3}','{4}','{5}','{6}',{7},{8},{9},{10},{11},{12},{13},{14})"
+				curs.execute("insert into absence.invoices(invoice_id,  company, invoice_batch, invoice_month, bill_date, items, id, billing_interval, payment_type, pricing_type, price_gross, invoice_counter, price_net, invoice_status, vat) values ('{0}','{1}','{2}','{3}','{4}','{5}','{6}',{7},{8},{9},{10},{11},{12},{13},{14})"
 				.format(invoice_id, company, invoice_batch, invoice_month, bill_date, items, _id, billing_interval, payment_type, pricing_type, price_gross, invoice_counter, price_net, invoice_status, vat))
 				conn.commit()
 
